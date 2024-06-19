31152c1a0a66e4f41ee76de3f1c822a978e9d8ba
gameTark <>
2024-06-19 21:57:21 +0900

    一旦保存

---
+++ src/components/navigations/horizontal-navigation/index.ts
@@ -0,0 +1,29 @@
+
+import { LitElement, css, html } from "lit";
+import { customElement } from "lit/decorators";
+
+@customElement('horizontal-navigation')
+export class HorizontalNavigation extends LitElement {
+  protected render() {
+    return html`
+<div class="wrap">
+  <nav class="header">
+    <slot name="navigation"></slot>
+  </nav>
+  <div class="body">
+    <slot name="body"></slot>
+  </div>
+</div>
+`
+  }
+
+  static styles = css`
+    :host {
+      margin: 0;
+      padding: 0;
+    }
+    .wrap {
+      display: flex;
+    }
+`
+}
---
+++ src/components/scroll-viewer/index.ts
@@ -0,0 +1,23 @@
+import { LitElement, css, html } from "lit";
+import { customElement } from "lit/decorators";
+
+@customElement('scroll-viewer')
+export class ScrollViewer extends LitElement {
+  protected render() {
+    return html`
+<div>
+  scroll-viewer
+  <slot></slot>
+</div>
+    `
+  }
+
+  static styles = css`
+    :host {
+      max-width: 1280px;
+      margin: 0 auto;
+      padding: 2rem;
+      text-align: center;
+    }
+`
+}
