5d4a30ab4839076e91b00a82596bd34becbe1db8
gameTark <>
2024-06-19 19:50:48 +0900

    feature: process envを設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "portfolio" : "",
   test: {
     environment: "jsdom",
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   }
 })
